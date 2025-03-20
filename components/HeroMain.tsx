16d2b12afe44d36fb12dabfc60618c1a7eafafab
Maxim Degtiarev <mxim.degt@gmail.com>
2025-03-20 16:46:48 -0300

    color

--- components/HeroMain.tsx
+++ components/HeroMain.tsx
@@ -6,14 +6,14 @@ const HeroMain = () => {
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 w-full justify-between lg:items-start overflow-y-hidden max-w-7xl md:mt-10 md:gap-30">
         <div>
-          <h1 className="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold leading-tight text-center md:text-left text-pretty">
+          <h1 className="my-4 text-3xl md:text-6xl text-white opacity-75 font-bold leading-tight text-center md:text-left text-pretty">
             Venta y reparaci&oacute;n de{" "}
             <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-500  via-pink-500 to-blue-600">
               computadoras
             </span>{" "}
             y m&aacute;s!
           </h1>
-          <p className="leading-normal text-base md:text-2xl mb-8 text-center md:text-pretty md:text-left ">
+          <p className="leading-normal text-white text-base md:text-2xl mb-8 text-center md:text-pretty md:text-left ">
             Te garantizamos productos de calidad y compatibilidad para que
             puedas trabajar sin problemas con tus dispositivos de cualquier
             marca.
